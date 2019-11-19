f05252ec09078bfe4dedf9f60b13668649d2f2f4
Man Darius <man@users.github.com>
2019-11-19 23:16:47 +0200

    Initial commit
    First push

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/***************************************************
+ *  Project: Maze Solver                           *
+ *  Authors: Man N. Darius,                        *
+ *           Pacuraru L. Fabin Virgil,             *
+ *           Zah D. Elena                          *
+ *  Description: The project is a maze generation  *
+ *               and solver.                       *
+ ***************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Work in progres.");
+    return 0;
+}
