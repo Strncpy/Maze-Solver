4025e76a27e2b70a908cdf3518a16a9509b8d010
Man Darius <man@users.github.com>
2019-12-06 17:28:53 +0200

    Fix big problem
    + post game menu(wip)
    
    Proiect aproape gata

--- headers/mat_gen.h
+++ headers/mat_gen.h
@@ -1,5 +1,6 @@
 #ifndef MAT_GEN_H_INCLUDED
 #define MAT_GEN_H_INCLUDED
+
 extern void mat(int n);
 extern int* matrix_s(int n);
 extern void first_step(int x,int y);
@@ -7,4 +8,5 @@ extern void path_an(int x,int y);
 extern int whtogo(int N,int V,int S,int E);
 extern void mov_arb(int s);
 extern int* gen_mat(int dim);
+
 #endif // MAT_GEN_H_INCLUDED
