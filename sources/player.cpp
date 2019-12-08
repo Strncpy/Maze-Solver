ffaf6cd33a89284c7bb196a4d935d39852d0767f
fabianut <fabianut@users.github.com>
2019-12-08 21:18:07 +0200

    Update player.cpp

--- sources/player.cpp
+++ sources/player.cpp
@@ -5,6 +5,44 @@
 #include "../headers/memoryalloc.h"
 #include "../headers/memoryalloc.h"
 
+void text(struct player_stats player)
+{
+    FILE *pf_ptr;
+    FILE *temp_ptr;
+    int scor, i=0, k=0;
+    char nume[20];
+    pf_ptr=fopen("players.txt", "a+");
+    temp_ptr=fopen("temp.txt", "w+");
+    while(fscanf(pf_ptr, "%s %d", nume, &scor) > 0)
+    {
+        if(player.score>scor)
+        {
+            if((i<10)&&(k<1))
+            {
+                fprintf(temp_ptr, "%s %d\n", player.name, player.score);
+                i+=1;
+                k+=1;
+            }
+            if(i<10)
+            {
+                fprintf(temp_ptr, "%s %d\n", nume, scor);
+                i+=1;
+            }
+        }
+        else if(i<10)
+        {
+            fprintf(temp_ptr, "%s %d\n", nume, scor);
+            i+=1;
+        }
+    }
+    if((fscanf(pf_ptr, "%s %d", nume, &scor) == 0)||(k==0))
+        fprintf(temp_ptr, "%s %d\n", player.name, player.score);
+    fclose(pf_ptr);
+    fclose(temp_ptr);
+    remove("players.txt");
+    rename("temp.txt", "players.txt");
+}
+
 int move_up(int *matrix,int n,int a,int b)
 {
     if(get_matrix_element( matrix,n,a-1,b)==0)
